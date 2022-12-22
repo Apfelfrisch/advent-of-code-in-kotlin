c7cf427bda5d19b77ca3b62374b15e978984cc2a
Nils Refle <n.refle@proengeno.de>
2022-12-22 11:39:12 +0100

    Day 1, Day 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,22 @@
+import java.io.File
+
+private fun lastItems(input: List<String>, number: Int): List<Int> {
+    return input.map { it.lines().sumOf { it.toInt() } }
+        .sortedDescending()
+        .take(number)
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return lastItems(input, 1).sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return lastItems(input, 3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    // val input = readInput("Day01")
+    val input = File("inputs/Day01.txt").readText().split("\n\n")
     part1(input).println()
     part2(input).println()
 }
---
+++ src/Day02.kt
@@ -0,0 +1,100 @@
+import Hand.*
+import java.io.File
+import kotlin.system.exitProcess
+
+enum class Hand(val score: Int) {
+    Rock(1),
+    Paper(2),
+    Scissors(3);
+}
+
+enum class Result(val score: Int) {
+    Loose(0),
+    Draw(3),
+    Win(6),
+}
+
+private fun Hand.winningHand(): Hand {
+    return when(this) {
+        Rock -> Scissors
+        Paper -> Rock
+        Scissors -> Paper
+    };
+}
+
+private fun Hand.loosingHand(): Hand {
+    return when(this) {
+        Rock -> Paper
+        Paper -> Scissors
+        Scissors -> Rock
+    };
+}
+
+private fun Char.toHand(): Hand {
+    return when (this) {
+        'A', 'X' -> Hand.Rock
+        'B', 'Y' -> Hand.Paper
+        'C', 'Z' -> Hand.Scissors
+        else -> {
+            error("Unknown Hand $this")
+        }
+    }
+}
+
+private fun Char.toResult(): Result {
+    return when (this) {
+        'X' -> Result.Loose
+        'Y' -> Result.Draw
+        'Z' -> Result.Win
+        else -> {
+            error("Unknown Result $this")
+        }
+    }
+}
+
+private fun myHand(opponentHand: Hand, myResult: Result): Hand {
+    return when(myResult) {
+        Result.Loose -> opponentHand.winningHand()
+        Result.Draw -> opponentHand
+        Result.Win -> opponentHand.loosingHand()
+    }
+}
+
+private fun Hand.playsAgainst(opponent: Hand): Result {
+
+    if (opponent == this) {
+        return Result.Draw
+    }
+
+    if (opponent.winningHand() == this) {
+        return Result.Loose
+    }
+
+    return Result.Win
+}
+
+private fun partOne(input: List<String>): Int {
+    return input.sumOf { game -> Int
+        val opponentsHand = game[0].toHand();
+        val myHand = game[2].toHand();
+
+        myHand.playsAgainst(opponentsHand).score + myHand.score
+    };
+}
+
+private fun partTwo(input: List<String>): Int {
+
+    return input.sumOf { game -> Int
+        val hand = game[0].toHand();
+        val result = game[2].toResult();
+
+        result.score + myHand(hand, result).score
+    }
+}
+
+fun main() {
+    val input = File("inputs/Day02.txt").readLines()
+
+    println(partOne(input))
+    println(partTwo(input))
+}
